ab366b77479e1b0cb283a584809fb0889b3d2db8
JuanD128 <juandavidgarciag@gmail.com>
2022-03-11 11:22:01 +0100

    ejercicio A clase

---
+++ week-3/InClass/exercise-A.js
@@ -0,0 +1,8 @@
+let Mesa1 = ["Jesus", "Maria", "Jose", "Katy", "Denisa"];
+let Mesa2 = ["Louis", "Anthony"]
+//Mesa.unshift("David"); se usa unshift para agregar este nombre al inicio de el arreglo
+Mesa1[0] = Mesa2[0]
+//array.push agrega un valor al final del arreglo
+Mesa1.push("Scott")
+console.log(Mesa1.length) 
+console.log(Mesa1);
